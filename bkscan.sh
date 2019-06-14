1b5103ed1bb2e531e47257c601f56b5746462c15
nikallass@yandex.ru <nikallass@yandex.ru>
2019-06-14 15:39:44 +0300

    Deleted not important docker arguments.

--- bkscan.sh
+++ bkscan.sh
@@ -68,14 +68,7 @@ then
     docker run -it --rm --privileged \
       --user=$USER \
       --env="DISPLAY" \
-      --workdir="/home/$USER" \
-      --volume="/home/$USER:/home/$USER" \
-      --volume="/etc/group:/etc/group:ro" \
-      --volume="/etc/passwd:/etc/passwd:ro" \
-      --volume="/etc/shadow:/etc/shadow:ro" \
-      --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-      --volume="$HOME/.Xauthority:$HOME/.Xauthority" \
       bkscan \
       xfreerdp /cve-2019-0708 /cert-ignore /v:${TARGET_IP}:${TARGET_PORT} /u:${RDP_USER} /p:${RDP_PASSWORD} ${DEBUG}
 else
@@ -83,14 +76,7 @@ else
     docker run -it --rm --privileged \
       --user=$USER \
       --env="DISPLAY" \
-      --workdir="/home/$USER" \
-      --volume="/home/$USER:/home/$USER" \
-      --volume="/etc/group:/etc/group:ro" \
-      --volume="/etc/passwd:/etc/passwd:ro" \
-      --volume="/etc/shadow:/etc/shadow:ro" \
-      --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-      --volume="$HOME/.Xauthority:$HOME/.Xauthority" \
       bkscan \
       xfreerdp /cve-2019-0708 /cert-ignore /v:${TARGET_IP}:${TARGET_PORT} -sec-nla /u:"" ${DEBUG}
 fi
